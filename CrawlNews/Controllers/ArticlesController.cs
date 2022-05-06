319ebeacf978f2f0cf76b6ae8d2957d42cf9a040
bqminh <64219602+BuiQuangMinh30@users.noreply.github.com>
2022-05-06 13:41:18 +0700

    previewlink

---
+++ CrawlNews/Controllers/ArticlesController.cs
@@ -0,0 +1,186 @@
+﻿using System;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web.Mvc;
+using CrawlNews.Models;
+using CrawlNews.config;
+using PagedList;
+
+namespace CrawlNews.Controllers
+{
+    public class ArticlesController : Controller
+    {
+        private DBConnection db = new DBConnection();
+
+        // GET: Articles
+        public ActionResult Index()
+        {
+            return View(db.Articles.ToList());
+        }
+        public ActionResult PreviewLink(string sortOrder, string currentFilter, string searchString, int? page, int? categoryId)
+        {
+            //ViewBag.ListCategory = db.Categories.ToList();
+
+            ViewBag.CurrentSort = sortOrder;
+            ViewBag.NameSortParm = String.IsNullOrEmpty(sortOrder) ? "name_desc" : "";
+
+            if (searchString != null)
+            {
+                page = 1;
+            }
+            else
+            {
+                searchString = currentFilter;
+            }
+
+            ViewBag.CurrentFilter = searchString;
+
+            var articles = db.Articles.AsQueryable();
+            if (!String.IsNullOrEmpty(searchString))
+            {
+                articles = articles.Where(s => s.Title.Contains(searchString));
+                                      
+            }
+            if (Request.QueryString["categoryId"] == null)
+            {
+                categoryId = -1;
+            }
+            ViewBag.CurrentCategory = categoryId;
+
+            if (categoryId != -1)
+            {
+                // tìm đến những Sources có CategoryId == ?
+                var sources = db.Sourses.ToList();
+
+                if (sources.Count != 0)
+                {
+                    foreach (var source in sources)
+                    {
+                        articles = articles.Where(s => s.Id.Equals(source.Id));
+                    }
+                }
+            }
+
+            switch (sortOrder)
+            {
+                case "name_desc":
+                    articles = articles.OrderByDescending(s => s.Title);
+                    break;
+                default:  // Name ascending 
+                    articles = articles.OrderBy(s => s.Title);
+                    break;
+            }
+            //articles.Include(a => a.Source);
+            int pageSize = 10;
+            int pageNumber = (page ?? 1);
+            return View(articles.ToPagedList(pageNumber, pageSize));
+        }
+
+
+        // GET: Articles/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Article article = db.Articles.Find(id);
+            if (article == null)
+            {
+                return HttpNotFound();
+            }
+            return View(article);
+        }
+
+        // GET: Articles/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Articles/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,UrlSource,Title,Image,Description,Content,CategoryId,CreatedAt")] Article article)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Articles.Add(article);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(article);
+        }
+
+        // GET: Articles/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Article article = db.Articles.Find(id);
+            if (article == null)
+            {
+                return HttpNotFound();
+            }
+            return View(article);
+        }
+
+        // POST: Articles/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,UrlSource,Title,Image,Description,Content,CategoryId,CreatedAt")] Article article)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(article).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(article);
+        }
+
+        // GET: Articles/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Article article = db.Articles.Find(id);
+            if (article == null)
+            {
+                return HttpNotFound();
+            }
+            return View(article);
+        }
+
+        // POST: Articles/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Article article = db.Articles.Find(id);
+            db.Articles.Remove(article);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ CrawlNews/Controllers/SoursesController.cs
@@ -0,0 +1,237 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Diagnostics;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Web;
+using System.Web.Mvc;
+using CrawlNews.config;
+using CrawlNews.Data;
+using CrawlNews.Models;
+using HtmlAgilityPack;
+using Newtonsoft.Json;
+using PagedList;
+using RabbitMQ.Client;
+
+namespace CrawlNews.Controllers
+{
+    public class SoursesController : Controller
+    {
+        private DBConnection db = new DBConnection();
+        public static HashSet<Sourse> setLink;
+       
+
+        // GET: Sourses
+        public ActionResult Index()
+        {
+            return View(db.Sourses.ToList());
+        }
+
+        // GET: Sourses/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Sourse sourse = db.Sourses.Find(id);
+            if (sourse == null)
+            {
+                return HttpNotFound();
+            }
+            return View(sourse);
+        }
+
+        // GET: Sourses/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Sourses/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,Url,SelectorSubUrl,SelectorTitle,SelectorImage,SelectorDescrition,SelectorContent,CategoryId")] Sourse sourse)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Sourses.Add(sourse);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(sourse);
+        }
+
+        // GET: Sourses/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Sourse sourse = db.Sourses.Find(id);
+            if (sourse == null)
+            {
+                return HttpNotFound();
+            }
+            return View(sourse);
+        }
+
+        // POST: Sourses/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Url,SelectorSubUrl,SelectorTitle,SelectorImage,SelectorDescrition,SelectorContent,CategoryId")] Sourse sourse)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(sourse).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(sourse);
+        }
+
+        // GET: Sourses/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Sourse sourse = db.Sourses.Find(id);
+            if (sourse == null)
+            {
+                return HttpNotFound();
+            }
+            return View(sourse);
+        }
+
+        // POST: Sourses/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Sourse sourse = db.Sourses.Find(id);
+            db.Sourses.Remove(sourse);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+        public ActionResult CheckSource()
+        {
+            return View();
+        }
+         public ActionResult PreviewLink()
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult CheckLink(Sourse sourceCheck)
+        {
+            if (sourceCheck.Url != "" )
+            {
+                try
+                {
+                    var web = new HtmlWeb();
+                    HtmlDocument doc = web.Load(sourceCheck.Url);
+                    var nodeList = doc.QuerySelectorAll(sourceCheck.Url);//tìm đến những thẻ a nằm trong h3 có class= title-news
+                    var setLinkSource = new HashSet<Sourse>(); // Đảm bảo link không giống nhau, nếu có sẽ bị ghi đè ở phần tử trước
+
+                    foreach (var node in nodeList)
+                    {
+                        var link = node.Attributes["href"]?.Value;
+                        if (string.IsNullOrEmpty(link)) // nếu link này null
+                        {
+                            continue;
+                        }
+                        var sourceCheck1 = new Sourse()
+                        {
+                            Url = link
+                        };
+
+                        setLinkSource.Add(sourceCheck1);
+                    }
+
+                    return PartialView("GetListSource", setLinkSource);
+                }
+                catch (Exception e)
+                {
+                    Debug.WriteLine("Error: " + e.Message);
+                    return PartialView("Faild");
+                }
+            }
+            return PartialView("Faild");
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Preview(Sourse sourceCheck)
+        {
+            if (sourceCheck.Url != "" && sourceCheck.SelectorTitle != "" && sourceCheck.SelectorDescrition != ""
+                && sourceCheck.SelectorContent != "" && sourceCheck.SelectorSubUrl != "" && sourceCheck.SelectorImage != "")
+            {
+                try
+                {
+                    Console.OutputEncoding = System.Text.Encoding.UTF8;
+                    var web = new HtmlWeb();
+                    HtmlDocument doc = web.Load(sourceCheck.Url);
+                    var title = doc.QuerySelector(sourceCheck.SelectorTitle).InnerText ?? "";
+                    var description = doc.QuerySelector(sourceCheck.SelectorDescrition).InnerText;
+                    var imageNode = doc.QuerySelector(sourceCheck.SelectorImage)?.Attributes["data-src"].Value;
+                    var content = doc.QuerySelector(sourceCheck.SelectorContent)?.InnerText;
+                    string thumbnail = "";
+                    if (imageNode != null)
+                    {
+                        thumbnail = imageNode;
+                    }
+                    else
+                    {
+                        thumbnail = "";
+                    }
+                    var contentNode = doc.QuerySelectorAll(sourceCheck.SelectorContent);
+                    StringBuilder contentBuilder = new StringBuilder();
+                    foreach (var content1 in contentNode)
+                    {
+                        contentBuilder.Append(content.ToString());
+                    }
+
+                    Article article = new Article()
+                    {
+                        Title = title,
+                        Description = description,
+                        Content = contentBuilder.ToString(),
+                        Image = thumbnail,
+                      
+                    };
+
+                    return PartialView("GetListSelector", article);
+                }
+                catch (Exception e)
+                {
+                    Debug.WriteLine("Error: " + e.Message);
+                    return PartialView("Faild");
+                }
+            }
+            return PartialView("Faild");
+        }
+    }
+}
